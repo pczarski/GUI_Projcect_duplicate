c588f77953b6c2999d3314f1110ab53ff03b9fd2
Przemyslaw Czarski <p.czarski@se19.qmul.ac.uk>
2020-03-16 20:13:41 +0000

    added the scroll with 3 hour slot weather forecasts

--- src/components/weather_home.js
+++ src/components/weather_home.js
@@ -24,6 +24,7 @@ export default class WeatherHome extends React.Component {
                         <div className="weather">
                             {weatherDescription}</div>
                     </div>
+
                 </div>
             </div>
 
